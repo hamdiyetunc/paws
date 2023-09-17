3b8d82a45ba3924ea75e9ff4ab3ad6bac4f0942f
Hamdiye Tunc <whamdiyetunc@outlook.com>
2023-09-17 20:58:06 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-let valueDisplays = document.querySelectorAll(".counter-num");
-let interval = 5000;
+document.addEventListener('DOMContentLoaded', function() {
+    let valueDisplays = document.querySelectorAll(".counter-num");
+    let interval = 5000;
 
-valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute
-    ("data-val"));
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function() {
-        startValue += 1;
-    valueDisplay.textContent = startValue;
-    if (startValue == endValue) {
-        clearInterval (counter);
-    }
-    }, duration);
+    valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        //also support decimals using parseFloat
+        let endValue = parseFloat(valueDisplay.getAttribute("data-val"));
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(function() {
+            startValue += 1;
+            valueDisplay.textContent = startValue;
+            //stop when startValue is greater than or equal to endValue
+            if (startValue >= endValue) {
+                clearInterval(counter);
+            }
+        }, duration);
+    });
 });
